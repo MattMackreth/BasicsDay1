d36c6a4af2026b4869988920652fca91a5993e28
Matthew Mackreth <MMackreth@spartaglobal.com>
2019-07-15 10:06:47 +0100

    variable answers

---
+++ 01variables.py
@@ -0,0 +1,6 @@
+#variables in Python
+my_variable = 'Hello there'
+#variable with string printed to console
+print(my_variable)
+#reassigning a variable
+my_variable = "I'm a new content in the same variable"
---
+++ variables.py
@@ -0,0 +1,7 @@
+# variables in Python
+my_variable = 'Hello there'
+# variable with string printed to console
+print(my_variable)
+# reassigning a variable
+my_variable = "I'm a new content in the same variable"
+print(my_variable)
