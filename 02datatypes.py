366686bd64abb8ca49c3224fd149b7a7d59ffd12
Matthew Mackreth <MMackreth@spartaglobal.com>
2019-07-15 10:43:59 +0100

    Variables and datatypes added

---
+++ 02datatypes.py
@@ -0,0 +1,37 @@
+# Data types
+# Computers are stupid
+# they don't understand context so we need to be specific with data types
+
+# We can use type() to check datatypes
+
+# Strings
+# lists of characters bundled together in a specific order
+# using index
+print('hello')
+print(type('hello'))
+
+# Concatentation of strings
+string_a = 'hello there'
+name_person = 'Juan Pier'
+print(string_a + ' ' + name_person)
+
+# Useful methods
+# Length
+# Returns the length of a string
+print(len(string_a))
+print(len(name_person))
+
+# Strip
+# Removes trailing or prevailing white spaces
+string_num = ' 90383 '
+print(string_num)
+print(string_num.strip())
+
+# Split
+string_text = 'Hello I need to test this'
+split_string = string_text.split(' ')
+print(split_string)
+# Casting
+
+
+
