45a368079ea67d82be618d23cb80b61b215c85f9
Matthew Mackreth <MMackreth@spartaglobal.com>
2019-07-15 12:12:05 +0100

    Casting added

---
+++ 04casting.py
@@ -0,0 +1,15 @@
+# Casting is when you change the datatype of an object
+# we can cast strings into integers or integers into strings
+
+# Casting String Class to Integer Class
+number_string = '10'
+print(type(number_string))
+# now changing to integer using int()
+number_int = int(number_string)
+print(type(int(number_int)))
+
+# Casting integer class to string class
+number_int2 = 11
+print(type(number_int2))
+number_string2 = str(number_int2)
+print(type(number_string2))
